7822b9ef6ba9979053910e6f5d2028b1c01d562e
rahull0328 <rmmehta2004@gmail.com>
2024-10-22 17:12:05 +0530

    Added Category Api

--- api/user/registerUser.php
+++ api/user/registerUser.php
@@ -2,11 +2,19 @@
 
     require '../../assets/includes/config.php';
 
-    $name = $_POST['name'];
+    $name = $_POST["name"];
     $email = $_POST['email'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-    $query = "INSERT INTO customers(`username`, `email`, `password`) VALUES('$name', '$email', '$password')";
+    $query = "INSERT INTO `customers`(`username`, `email`, `password`) VALUES('$name', '$email', '$password')";
     $result = mysqli_query($con, $query);
+    header("Location: ../../pages/login.php");
+    
+    if($result){
+        $response = [
+            "success" => true,
+            "message" => "Registration successful!"
+        ];
+    }
 
     mysqli_close($con);
