ce729b7e2152b145a36f6b3491a16862e546b993
rahull0328 <rmmehta2004@gmail.com>
2024-11-01 20:15:28 +0530

    Changes to Single Product Page

--- pages/product-single.php
+++ pages/product-single.php
@@ -53,7 +53,7 @@ $data = mysqli_fetch_all($result);
 									</button>
 								</span>
 								<input type="text" id="quantity" name="quantity" class="form-control input-number" value="1" min="1" max="100">
-								<input type="text" value="<?= $id ?>" class="form-control input-number" name="productId" id="productId">
+								<input type="hidden" value="<?= $id ?>" class="form-control input-number" name="productId" id="productId">
 								<span class="input-group-btn ml-2">
 									<button type="button" class="quantity-right-plus btn" data-type="plus" data-field="">
 										<i class="icon-plus"></i>
