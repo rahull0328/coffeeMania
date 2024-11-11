847140e0c486add8c5b02378d8805494f0c8ccd6
rahull0328 <rmmehta2004@gmail.com>
2024-11-11 19:32:45 +0530

    Working on Profile Page

--- pages/profile.php
+++ pages/profile.php
@@ -154,20 +154,6 @@ $data = mysqli_fetch_all($result);
         }
     </style>
 </head>
-<!-- show insert model -->
-<dialog id="insertCategoryModal" style="top: 50%;left: 50%;border:0px;border-radius:10px;height: 50%;width: 30%;-webkit-transform: translateX(-50%) translateY(-50%);-moz-transform: translateX(-50%) translateY(-50%);-ms-transform: translateX(-50%) translateY(-50%);transform: translateX(-50%) translateY(-50%);">
-    <form method="POST" id="insertForm">
-        <div class="card">
-            <div class="card-body">
-                <div class="mb-3">
-                    <label for="" class="form-label mb-3">Category Name : </label>
-                    <input type="text" class="form-control" id="categoryName" name="categoryName" placeholder="Enter Category Name : " required>
-                </div>
-                <button type="submit" class="btn btn-primary" name="submit">Submit</button>
-            </div>
-        </div>
-    </form>
-</dialog>
 
 <section class="home-slider owl-carousel">
 
@@ -200,11 +186,10 @@ $data = mysqli_fetch_all($result);
                                     </div>
                                     <h6 class="f-w-600"><?= $data[$i][1] ?></h6>
                                     <a class="mb-0 fw-normal align-middle">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="col icon icon-tabler icon-tabler-plus" onclick="showInsertModal()" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                            <path d="M12 5l0 14"></path>
-                                            <path d="M5 12l14 0"></path>
-                                        </svg>
+                                        <!-- Button trigger modal -->
+                                        <button type="button" class="btn btn-primary" data-toggle="modal">
+                                            Update Profile
+                                        </button>
                                     </a>
                                 </div>
                             </div>
@@ -237,10 +222,30 @@ $data = mysqli_fetch_all($result);
     </div>
 </div>
 
+<!-- Modal -->
+<div class="modal fade" id="updateProfileModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
 <script src="../assets/js/jquery-3.6.0.min.js"></script>
 <script>
-    function showInsertModal() {
-        insertCategoryModal.showModal();
+    function showModal() {
+        updateProfileModal.showModal();
     }
 </script>
 <?php
